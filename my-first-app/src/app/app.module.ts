0256be453511fb462e26e6f7f53fce8b733a1ddf
kratos619 <gaurav.palaspagar@hotmail.com>
2018-03-05 23:01:59 +0530

    adding recipes-list component
    adding recipes-list omponent into recipes component
    cli cmd to creat component within component folder
    ng c g recipes/recipes-list --spec false

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
+import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     RecipesComponent,
+    RecipesListComponent,
   ],
   imports: [
     BrowserModule,
