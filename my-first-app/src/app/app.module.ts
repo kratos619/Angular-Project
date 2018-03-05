eeef389f3257ede60607b25926671d5eb98be602
kratos619 <gaurav.palaspagar@hotmail.com>
2018-03-05 22:56:14 +0530

    adding recipes component

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    RecipesComponent,
   ],
   imports: [
     BrowserModule,
