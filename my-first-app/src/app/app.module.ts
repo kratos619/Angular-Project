7b7f90afa528800e79e7f92326c13c8f71bc13ea
kratos619 <gaurav.palaspagar@hotmail.com>
2018-03-05 22:26:48 +0530

    application set up

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -1,22 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.componant';
-import { ServersComponent } from './servers/servers.component';
-
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-   
   ],
   providers: [],
   bootstrap: [AppComponent]
