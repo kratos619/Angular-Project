c0b7d518ed2ba7971289c5261ad9f1a0835e36db
kratos619 <gaurav.palaspagar@hotmail.com>
2018-04-08 16:13:00 +0530

    testing string and method interpoletions

--- AngularApp2/src/app/sandbox/sandbox.component.ts
+++ AngularApp2/src/app/sandbox/sandbox.component.ts
@@ -3,18 +3,35 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sandbox',
   templateUrl: './sandbox.component.html',
-  styleUrls: ['./sandbox.component.css']
+  styleUrls: ['./sandbox.component.css'],
+  template : `
+  <h3>{{printnumArray()}}</h3>
+  <h3>{{printstring()}}</h3>
+  <h3>{{printanyArry()}}</h3>
+  `
 })
 export class SandboxComponent implements OnInit {
 
-  name  = "gaurav";
+  name: String  = 'gaurav';
+numberArray:number[] = [1,2,3,4];
+Stringarray:string[] = ['gaurav','milind','palaspagar'];
+anyarray:any[] =  [1,'gaurav',false];
 
-  person ={
-    firstName :'gaurav',
-    sencondName : 'ingle'
-  }
-  constructor() { }
-
+  person = {
+    firstName : 'gaurav',
+    sencondName : 'ingle',
+  };
+  constructor() {
+   }
+printnumArray(){
+  return this.numberArray;
+}
+printstring(){
+  return this.Stringarray;
+}
+printanyArry(){
+  return this.anyarray;
+}
   ngOnInit() {
   }
 
