e42cfe09427f9bc3d7d786c9f969776aa5792798
kratos619 <gaurav.palaspagar@hotmail.com>
2018-04-19 05:32:34 +0530

    Angular Refresh, conditions,

--- AngularApp2/src/app/sandbox/sandbox.component.ts
+++ AngularApp2/src/app/sandbox/sandbox.component.ts
@@ -10,61 +10,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SandboxComponent implements OnInit {
 
-  name: String  = 'gaurav';
-numberArray:number[] = [1,2,3,4];
-Stringarray:string[] = ['gaurav','milind','palaspagar'];
-anyarray:any[] =  [1,'gaurav',false];
-
-  person = {
-    firstName : 'gaurav',
-    sencondName : 'ingle',
-  };
-
-  customer : Customer;
-
-   customerArra:Customer[];
-  
-  constructor() {
-     this.customer = {
-      id: 1,
-      name:'gaurav',
-      email : 'mail@gmil.com'
-    }
-    // call customerrs array
-    this.customerArra = [
-      {
-        id: 1,
-        name: 'gaurav',
-        email: 'mail@gmil.com'
-      },
-      {
-        id: 2,
-        name: 'num2',
-        email: 'mail@gmil.com'
-      },
-      {
-        id: 3,
-        name: 'dsdad',
-        email: 'numthree@gmil.com'
-      },
-      {
-        id: 4,
-        name: 'rajeev',
-        email: 'rajeev@gmil.com'
-      }
-
-    ];
-   }
-   
-printnumArray(){
-  return this.numberArray;
-}
-printstring(){
-  return this.Stringarray;
-}
-printanyArry(){
-  return this.anyarray;
-}
   ngOnInit() {
   }
 
