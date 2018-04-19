2ae3d95363b8b77e01d91e7ff112942337033c64
kratos619 <gaurav.palaspagar@hotmail.com>
2018-04-19 05:50:34 +0530

    condition if-else { ngIf : else}

--- AngularApp2/src/app/sandbox/sandbox.component.ts
+++ AngularApp2/src/app/sandbox/sandbox.component.ts
@@ -1,16 +1,12 @@
 import { Customer } from './customer';
 import { Component, OnInit } from '@angular/core';
- 
+
 @Component({
   selector: 'app-sandbox',
-  templateUrl: './sandbox.component.html',
   styleUrls: ['./sandbox.component.css'],
-
-
+  templateUrl: './sandbox.component.html'
 })
 export class SandboxComponent implements OnInit {
-
-  ngOnInit() {
-  }
-
+  age: number = 16;
+  ngOnInit() {}
 }
