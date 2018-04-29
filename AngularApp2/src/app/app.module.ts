02f019a5e9f8b34f53abb260f781f3fac93ddb4f
kratos619 <gaurav.palaspagar@hotmail.com>
2018-04-29 12:53:19 +0530

    Html Routers With navbars

--- AngularApp2/src/app/app.module.ts
+++ AngularApp2/src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { SandboxComponent } from './sandbox/sandbox.component';
 import { ServicesComponent } from './services/services.component';
+import { HomeComponent } from './Components/home/home.component';
+import { AboutComponent } from './Components/about/about.component';
+import {RouterModule, Routes} from '@angular/router';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+
+const appRouter : Routes = [
+  { path:'',component:HomeComponent},
+  { path:'about',component:AboutComponent}
+
+]
 
 @NgModule({
-  declarations: [AppComponent, SandboxComponent],
-  imports: [BrowserModule, FormsModule, HttpModule],
+  declarations: [AppComponent, SandboxComponent, HomeComponent, AboutComponent, NavbarComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRouter)
+  ],
   providers: [ServicesComponent],
   bootstrap: [AppComponent]
 })
