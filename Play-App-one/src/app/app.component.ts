14011d4bc0d45290c1611958376fddbb28772fb1
Gaurav Palaspagar <gaurav.palaspagar@hotmail.com>
2020-08-24 19:46:32 +0530

    create library

--- Play-App-one/src/app/app.component.ts
+++ Play-App-one/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-
+import {LoggerService} from 'my-lib';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Play-App-one';
+  constructor(logger: LoggerService) {
+    logger.log('hello world')
+  }
 }
