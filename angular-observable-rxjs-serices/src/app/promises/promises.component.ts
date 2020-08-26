7fc59eb325a00fac3477ffbe08626aa093037736
Gaurav Palaspagar <gaurav.palaspagar@hotmail.com>
2020-08-26 20:44:09 +0530

    create promis component

---
+++ angular-observable-rxjs-serices/src/app/promises/promises.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-promises',
+  templateUrl: './promises.component.html',
+  styleUrls: ['./promises.component.css']
+})
+export class PromisesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
