a4ae59fbb434353915a41154e146d7a1d7e55f3b
Gaurav Palaspagar <gaurav.palaspagar@hotmail.com>
2020-08-26 20:46:20 +0530

    add routing to app module and add ob module

--- angular-observable-rxjs-serices/src/app/app-routing.module.ts
+++ angular-observable-rxjs-serices/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ObservablesComponent } from './observables/observables.component';
+import { PromisesComponent } from './promises/promises.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'promises', component: PromisesComponent },
+  // { path: '**', redirectTo: 'observables' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular-observable-rxjs-serices/src/app/app.module.ts
+++ angular-observable-rxjs-serices/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ObservablesComponent } from './observables/observables.component';
+import { HeaderComponent } from './header/header.component';
+import { PromisesComponent } from './promises/promises.component';
+import { ObservableModule } from './observables/observable.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ObservablesComponent,
+    HeaderComponent,
+    PromisesComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ObservableModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
