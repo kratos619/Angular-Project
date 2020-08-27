2acd6c1686dcf673995c6bd37e55431bb926ac6e
Gaurav Palaspagar <gaurav.palaspagar@hotmail.com>
2020-08-27 12:59:29 +0530

    load component intervel

--- angular-observable-rxjs-serices/src/app/observables/observable.module.ts
+++ angular-observable-rxjs-serices/src/app/observables/observable.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { ObservableRoutingModule } from './observable-routing.module';
 import { ObservablesListComponent } from './observables-list/observables-list.component';
 import { FromEventComponent } from './observables-list/from-event/from-event.component';
+import { IntervalComponent } from './observables-list/interval/interval.component';
 
 
 @NgModule({
-  declarations: [ObservablesListComponent, FromEventComponent],
+  declarations: [ObservablesListComponent, FromEventComponent, IntervalComponent],
   imports: [
     CommonModule,
     ObservableRoutingModule
