53827ef2981f217c7a5d70e258aff7e83c7cef11
Gaurav Palaspagar <gaurav.palaspagar@hotmail.com>
2020-08-27 16:29:12 +0530

    add of operator part one

--- angular-observable-rxjs-serices/src/app/observables/observables-list/from-of-operator/from-of-operator.component.ts
+++ angular-observable-rxjs-serices/src/app/observables/observables-list/from-of-operator/from-of-operator.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {of} from 'rxjs';
+import {FromeventService} from '../../../appservice/fromevent.service';
 
 @Component({
   selector: 'app-from-of-operator',
   templateUrl: './from-of-operator.component.html',
   styleUrls: ['./from-of-operator.component.css']
 })
-export class FromOfOperatorComponent implements OnInit {
+export class FromOfOperatorComponent implements OnInit , AfterViewInit {
 
-  constructor() { }
+  @ViewChild('fromOfOpOne') fromOfOpOne : ElementRef;
+   objectData : object;
+  constructor(private _appendData : FromeventService) { }
 
   ngOnInit(): void {
+    let data = {
+      name : 'gaurav',
+      lname : "palaspagar",
+      age : 26
+    }
+    let ofOpObj = of(data);
+    ofOpObj.subscribe(
+      (res) => {
+        this.objectData = res;
+        console.log("objectData",this.objectData);
+        // this._appendData.appendData(,this.fromOfOpOne)
+      }
+    )
+  }
+
+  ngAfterViewInit(): void {
+    let ofOp = of('data one','data two','data three');
+
+    ofOp.subscribe(
+      (res) => {
+        this._appendData.appendData(res,this.fromOfOpOne)
+      }
+    );
+
+
+
+
   }
 
 }
