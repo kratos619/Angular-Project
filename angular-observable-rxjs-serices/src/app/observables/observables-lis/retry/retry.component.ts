6ae3b4d3de43fe37fd53a27a07a3a536ef2b060e
Gaurav Palaspagar <gaurav.palaspagar@hotmail.com>
2020-08-29 21:33:41 +0530

    create retry component

---
+++ angular-observable-rxjs-serices/src/app/observables/observables-lis/retry/retry.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-retry',
+  templateUrl: './retry.component.html',
+  styleUrls: ['./retry.component.css']
+})
+export class RetryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
