b10ae1ee7982bfefcdeb59444ec1b78d41a4758b
Gaurav Palaspagar <gaurav.palaspagar@hotmail.com>
2020-09-01 13:30:37 +0530

    fix services

--- angularAndLaravel/frontend/src/app/components/login/login.component.ts
+++ angularAndLaravel/frontend/src/app/components/login/login.component.ts
@@ -1,4 +1,4 @@
-import { AuthService } from './../../auth/auth.service';
+import { AuthService } from 'src/app/service/auth/auth.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
--- angularAndLaravel/frontend/src/app/componentss/signup/signup.component.ts
+++ angularAndLaravel/frontend/src/app/componentss/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from 'src/app/auth/auth.service';
+import { AuthService } from 'src/app/service/auth/auth.service';
 
 @Component({
   selector: 'app-signup',
