d123acbc5943bbe8d924baa79813d658a4a85eba
Gaurav Palaspagar <gaurav.palaspagar@hotmail.com>
2020-09-01 09:43:48 +0530

    create componet signup

--- angularAndLaravel/frontend/src/app/app.module.ts
+++ angularAndLaravel/frontend/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './componentss/signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    LoginComponent
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
