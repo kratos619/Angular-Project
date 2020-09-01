52ce03ea98e1c7d311e235888e261006888dddc6
Gaurav Palaspagar <gaurav.palaspagar@hotmail.com>
2020-09-01 22:24:03 +0530

    update guards

--- angularAndLaravel/frontend/src/app/components/navbar/navbar.component.ts
+++ angularAndLaravel/frontend/src/app/components/navbar/navbar.component.ts
@@ -1,27 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/service/auth/auth.service';
+import { TokenService } from 'src/app/service/Token/token.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
+  public loggedIn: boolean;
 
-  public loggedIn : boolean;
-
-
-  constructor(private _authService : AuthService) { }
+  constructor(private _authService: AuthService,private _tokenService : TokenService,private _router : Router) {}
 
   ngOnInit(): void {
-    this._authService.authStatus.subscribe(
-      (res) => {
-        this.loggedIn = res;
-        console.log('a',res);
-
-      }
-    );
-
+    this._authService.authStatus.subscribe((res) => {
+      this.loggedIn = res;
+      console.log('a', res);
+    });
   }
 
+  loggout(event:Event){
+      event.preventDefault();
+      this._tokenService.remove();
+      this._authService.changeAuthStatus(false);
+      this._router.navigateByUrl('/login');
+      console.log(event);
+
+
+  }
 }
