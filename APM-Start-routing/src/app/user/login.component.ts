f3a9229de60527d6c629d62777db11b6be1719c9
Gaurav Palaspagar <gaurav.palaspagar@hotmail.com>
2020-09-07 23:47:26 +0530

    add logout

--- APM-Start-routing/src/app/user/login.component.ts
+++ APM-Start-routing/src/app/user/login.component.ts
@@ -2,15 +2,16 @@ import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
 
 @Component({
-  templateUrl: './login.component.html'
+  templateUrl: './login.component.html',
 })
 export class LoginComponent {
   errorMessage: string;
   pageTitle = 'Log In';
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private _route: Router) {}
 
   login(loginForm: NgForm): void {
     if (loginForm && loginForm.valid) {
@@ -19,8 +20,14 @@ export class LoginComponent {
       this.authService.login(userName, password);
 
       // Navigate to the Product List page after log in.
+      this._route.navigate(['/products']);
     } else {
       this.errorMessage = 'Please enter a user name and password.';
     }
+  }
+
+  logout() {
+    this.authService.logout();
+    this._route.navigate(['/']);
   }
 }
