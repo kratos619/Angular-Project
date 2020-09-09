1e875a31e8f05ad277cf716273c6263798e493a6
Gaurav Palaspagar <gaurav.palaspagar@hotmail.com>
2020-09-09 09:11:09 +0530

    :grapes:  group routes

--- APM-Start-routing/src/app/products/product.module.ts
+++ APM-Start-routing/src/app/products/product.module.ts
@@ -14,29 +14,37 @@ import { RouterModule } from '@angular/router';
   imports: [
     SharedModule,
     RouterModule.forChild([
-      { path: 'products', component: ProductListComponent },
       {
-        path: 'products/:id',
-        component: ProductDetailComponent,
-        resolve: { resolveData: ProductResolverService },
-      },
-      {
-        path: 'products/:id/edit',
-        component: ProductEditComponent,
-        resolve: { resolveData: ProductResolverService },
+        path: 'products',
         children: [
           {
             path: '',
-            redirectTo: 'info',
-            pathMatch: 'full',
+            component: ProductListComponent,
           },
           {
-            path: 'info',
-            component: ProductEditInfoComponent,
+            path: ':id',
+            component: ProductDetailComponent,
+            resolve: { resolveData: ProductResolverService },
           },
           {
-            path: 'tags',
-            component: ProductEditTagsComponent,
+            path: ':id/edit',
+            component: ProductEditComponent,
+            resolve: { resolveData: ProductResolverService },
+            children: [
+              {
+                path: '',
+                redirectTo: 'info',
+                pathMatch: 'full',
+              },
+              {
+                path: 'info',
+                component: ProductEditInfoComponent,
+              },
+              {
+                path: 'tags',
+                component: ProductEditTagsComponent,
+              },
+            ],
           },
         ],
       },
