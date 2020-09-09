8ddc966bfa5bea894a0c7544a1e1db1030fd4b51
Gaurav Palaspagar <gaurav.palaspagar@hotmail.com>
2020-09-09 08:39:32 +0530

    :+1:  pass data to child component

--- APM-Start-routing/src/app/products/product-edit/product-edit-info.component.ts
+++ APM-Start-routing/src/app/products/product-edit/product-edit-info.component.ts
@@ -1,3 +1,4 @@
+// import { Product } from './../product';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { NgForm } from '@angular/forms';
@@ -11,14 +12,13 @@ export class ProductEditInfoComponent implements OnInit {
   @ViewChild(NgForm, { static: false }) productForm: NgForm;
 
   errorMessage: string;
-  product = {
-    id: 1,
-    productName: 'test',
-    productCode: 'test',
-    description: 'test',
-  };
+  product: Product;
 
   constructor(private route: ActivatedRoute) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.route.parent.data.subscribe((result) => {
+      this.product = result['resolveData'];
+    });
+  }
 }
