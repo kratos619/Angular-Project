ffa3258f8e067b7673a1bfaf7021f8e9f863148c
Gaurav Palaspagar <gaurav.palaspagar@hotmail.com>
2020-09-11 11:49:15 +0530

    add dynamic form

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit, ViewChild, AfterViewInit } from "@angular/core";
-import { NgForm, FormGroup, FormControl, Validators } from "@angular/forms";
+import {
+  NgForm,
+  FormGroup,
+  FormControl,
+  Validators,
+  FormArray,
+} from "@angular/forms";
 
 @Component({
   selector: "app-root",
@@ -11,13 +17,20 @@ export class AppComponent implements OnInit, AfterViewInit {
   ngOnInit(): void {
     // throw new Error("Method not implemented.");
     this.signUpFrom = new FormGroup({
-      username: new FormControl(null, [
-        Validators.required,
-        Validators.minLength(3),
-      ]),
-      email: new FormControl(null, [Validators.email, Validators.required]),
+      userData: new FormGroup({
+        username: new FormControl(null, [
+          Validators.required,
+          Validators.minLength(3),
+        ]),
+        email: new FormControl(null, [Validators.email, Validators.required]),
+      }),
       secret: new FormControl(null, Validators.required),
+      hobbies: new FormArray([]),
     });
+  }
+  addHobby() {
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.signUpFrom.get("hobbies")).push(control);
   }
   ngAfterViewInit() {
     // this.signupForm.value.username = "gaurav";
