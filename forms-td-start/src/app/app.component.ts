199e12ab492ae4aa8fa3a75d8775efe7616c66a5
Gaurav Palaspagar <gaurav.palaspagar@hotmail.com>
2020-09-11 09:06:23 +0530

    resset for reactive forms

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -3,38 +3,13 @@ import { NgForm } from "@angular/forms";
 
 @Component({
   selector: "app-root",
-  templateUrl: "./app.component.html",
+  templateUrl: "./reactiveform.component.html",
   styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements OnInit, AfterViewInit {
   ngOnInit(): void {
     // throw new Error("Method not implemented.");
   }
-  @ViewChild("form", { static: false }) signupForm: NgForm;
-
-  suggestUserName() {
-    this.signupForm.form.patchValue({
-      username: "gaurav",
-      email: "gaurav@mail.com",
-      secret: "pet",
-    });
-  }
-  fields = [
-    {
-      name: "start",
-    },
-  ];
-  onSubmit(form: NgForm) {
-    console.log(form);
-  }
-
-  addField() {
-    console.log("hi");
-    this.fields.push({
-      name: "a",
-    });
-  }
-
   ngAfterViewInit() {
     // this.signupForm.value.username = "gaurav";
   }
