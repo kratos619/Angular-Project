edc156edf550c1569880d792755bab65c4b99534
Gaurav Palaspagar <gaurav.palaspagar@hotmail.com>
2020-09-11 10:45:05 +0530

    :star2:  reactive form basic validation

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild, AfterViewInit } from "@angular/core";
-import { NgForm } from "@angular/forms";
+import { NgForm, FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
   selector: "app-root",
@@ -7,10 +7,22 @@ import { NgForm } from "@angular/forms";
   styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements OnInit, AfterViewInit {
+  signUpFrom: FormGroup;
   ngOnInit(): void {
     // throw new Error("Method not implemented.");
+    this.signUpFrom = new FormGroup({
+      username: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+      email: new FormControl(null, [Validators.email, Validators.required]),
+      secret: new FormControl(null, Validators.required),
+    });
   }
   ngAfterViewInit() {
     // this.signupForm.value.username = "gaurav";
+  }
+  onsubmit() {
+    console.log(this.signUpFrom);
   }
 }
