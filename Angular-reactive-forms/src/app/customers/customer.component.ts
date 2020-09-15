c1c05c6e3b8fd633709eed7e5dea63771be01b68
Gaurav Palaspagar <gaurav.palaspagar@hotmail.com>
2020-09-15 12:13:10 +0530

    :fire: add reactive form

--- Angular-reactive-forms/src/app/customers/customer.component.ts
+++ Angular-reactive-forms/src/app/customers/customer.component.ts
@@ -1,23 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 
 import { Customer } from './customer';
 
 @Component({
   selector: 'app-customer',
   templateUrl: './customer.component.html',
-  styleUrls: ['./customer.component.css']
+  styleUrls: ['./customer.component.css'],
 })
 export class CustomerComponent implements OnInit {
   customer = new Customer();
-
-  constructor() { }
+  customerForm: FormGroup;
+  constructor() {}
 
   ngOnInit(): void {
+    this.customerForm = new FormGroup({
+      firstName: new FormControl(null),
+      lastName: new FormControl(),
+      email: new FormControl(),
+      sendCatalog: new FormControl(true),
+    });
   }
 
-  save(customerForm: NgForm): void {
-    console.log(customerForm.form);
-    console.log('Saved: ' + JSON.stringify(customerForm.value));
+  save(): void {
+    console.log(this.customerForm);
+    console.log('Saved: ' + JSON.stringify(this.customerForm));
   }
 }
