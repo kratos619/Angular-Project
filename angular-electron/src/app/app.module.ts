04c7bd4a667365a87bf5765c5bf48efe1741310b
Gaurav Palaspagar <gaurav.palaspagar@hotmail.com>
2020-09-24 15:08:14 +0530

    moves file to share modules

--- angular-electron/src/app/app.module.ts
+++ angular-electron/src/app/app.module.ts
@@ -3,7 +3,6 @@ import "../polyfills";
 
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
 import { HttpClientModule, HttpClient } from "@angular/common/http";
 import { CoreModule } from "./core/core.module";
 import { SharedModule } from "./shared/shared.module";
@@ -15,27 +14,20 @@ import { TranslateModule, TranslateLoader } from "@ngx-translate/core";
 import { TranslateHttpLoader } from "@ngx-translate/http-loader";
 
 import { AppComponent } from "./app.component";
-import { MatSliderModule } from "@angular/material/slider";
 
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
   return new TranslateHttpLoader(http, "./assets/i18n/", ".json");
 }
-import { MatInputModule } from "@angular/material/input";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule,
     CoreModule,
-    SharedModule,
     AppRoutingModule,
-    MatSliderModule,
-    MatInputModule,
-    BrowserAnimationsModule,
+    SharedModule,
     TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
--- angular-electron/src/app/shared/shared.module.ts
+++ angular-electron/src/app/shared/shared.module.ts
@@ -1,15 +1,32 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule } from "@ngx-translate/core";
 
-import { PageNotFoundComponent } from './components/';
-import { WebviewDirective } from './directives/';
-import { FormsModule } from '@angular/forms';
+import { PageNotFoundComponent } from "./components/";
+import { WebviewDirective } from "./directives/";
+import { FormsModule } from "@angular/forms";
+import { MatInputModule } from "@angular/material/input";
+import { MatSliderModule } from "@angular/material/slider";
 
 @NgModule({
   declarations: [PageNotFoundComponent, WebviewDirective],
-  imports: [CommonModule, TranslateModule, FormsModule],
-  exports: [TranslateModule, WebviewDirective, FormsModule]
+  imports: [
+    CommonModule,
+    TranslateModule,
+    FormsModule,
+    MatSliderModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+  ],
+  exports: [
+    TranslateModule,
+    WebviewDirective,
+    FormsModule,
+    MatSliderModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+  ],
 })
 export class SharedModule {}
