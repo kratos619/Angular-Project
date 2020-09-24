52f18c72d1e6ada71816c6d0025dc972866b345b
Gaurav Palaspagar <gaurav.palaspagar@hotmail.com>
2020-09-24 23:24:05 +0530

    app failur

--- angular-electron/src/app/shared/shared.module.ts
+++ angular-electron/src/app/shared/shared.module.ts
@@ -1,9 +1,7 @@
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-
 import { TranslateModule } from "@ngx-translate/core";
-
 import { PageNotFoundComponent } from "./components/";
 import { WebviewDirective } from "./directives/";
 import { FormsModule } from "@angular/forms";
