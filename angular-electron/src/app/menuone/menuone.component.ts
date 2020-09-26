816739dc72f70baecfb5aae969d91601561aff3a
Gaurav Palaspagar <gaurav.palaspagar@hotmail.com>
2020-09-26 22:33:21 +0530

    :fire: add take screnshot

--- angular-electron/src/app/menuone/menuone.component.ts
+++ angular-electron/src/app/menuone/menuone.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ElectronService } from "app/core/services";
 
 @Component({
-  selector: 'app-menuone',
-  templateUrl: './menuone.component.html',
-  styleUrls: ['./menuone.component.scss']
+  selector: "app-menuone",
+  templateUrl: "./menuone.component.html",
+  styleUrls: ["./menuone.component.scss"],
 })
 export class MenuoneComponent implements OnInit {
+  constructor(private _electronServices: ElectronService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  takeScrenShot() {
+    this._electronServices.takeScreenShot();
   }
-
 }
