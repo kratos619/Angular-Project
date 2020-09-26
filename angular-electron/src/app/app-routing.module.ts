51b76929d36b8544e708a69b9950d6ba2608031f
Gaurav Palaspagar <gaurav.palaspagar@hotmail.com>
2020-09-26 09:27:59 +0530

    sidenav added

--- angular-electron/src/app/app-routing.module.ts
+++ angular-electron/src/app/app-routing.module.ts
@@ -1,29 +1,50 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { PageNotFoundComponent } from './shared/components';
-import {MenuoneComponent} from "./menuone/menuone.component";
-
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { PageNotFoundComponent } from "./shared/components";
+import { MenuoneComponent } from "./menuone/menuone.component";
 
 const routes: Routes = [
   {
-    path : "homeOne",
-    component : MenuoneComponent
+    path: "homeOne",
+    component: MenuoneComponent,
   },
   {
-    path: '',
-    redirectTo: 'homeOne',
-    pathMatch: 'full'
+    path: "assignment",
+    component: MenuoneComponent,
   },
   {
-    path: '**',
-    component: PageNotFoundComponent
-  }
+    path: "mail",
+    component: MenuoneComponent,
+  },
+  {
+    path: "media",
+    component: MenuoneComponent,
+  },
+  {
+    path: "setting",
+    component: MenuoneComponent,
+  },
+  {
+    path: "account",
+    component: MenuoneComponent,
+  },
+  {
+    path: "chat",
+    component: MenuoneComponent,
+  },
+  {
+    path: "",
+    redirectTo: "homeOne",
+    pathMatch: "full",
+  },
+  {
+    path: "**",
+    component: PageNotFoundComponent,
+  },
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes),
-  ],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
